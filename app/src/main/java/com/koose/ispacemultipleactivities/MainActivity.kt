10469dcf081cd3d7b0a63b6b0637c4c839fb037a
Koose <KooseTheBlogger@gmail.com>
2022-01-26 19:41:38 +0000

    create a var to hold the button instance to findViewbyID

--- app/src/main/java/com/koose/ispacemultipleactivities/MainActivity.kt
+++ app/src/main/java/com/koose/ispacemultipleactivities/MainActivity.kt
@@ -2,8 +2,11 @@ package com.koose.ispacemultipleactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn_5: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

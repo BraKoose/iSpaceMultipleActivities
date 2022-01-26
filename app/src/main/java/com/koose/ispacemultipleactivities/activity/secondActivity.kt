aa69b5f8ad3af733e55b112d03fabe6b9b2edfee
Koose <KooseTheBlogger@gmail.com>
2022-01-26 19:34:49 +0000

    Initial commit

---
+++ app/src/main/java/com/koose/ispacemultipleactivities/activity/secondActivity.kt
@@ -0,0 +1,12 @@
+package com.koose.ispacemultipleactivities.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.koose.ispacemultipleactivities.R
+
+class secondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}

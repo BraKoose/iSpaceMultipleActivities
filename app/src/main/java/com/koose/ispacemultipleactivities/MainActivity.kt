811a4c540c6f615e690211f82a29e8209463b759
Koose <KooseTheBlogger@gmail.com>
2022-01-26 19:48:07 +0000

    creating an intent

--- app/src/main/java/com/koose/ispacemultipleactivities/MainActivity.kt
+++ app/src/main/java/com/koose/ispacemultipleactivities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.koose.ispacemultipleactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.koose.ispacemultipleactivities.activity.secondActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btn_5: Button
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_5 = findViewById(R.id.button5)
+        btn_5.setOnClickListener {
+            val intent = Intent(this, secondActivity::class.java)
+        }
+
     }
 }
